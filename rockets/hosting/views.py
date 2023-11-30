1ac211c849613e913f9573bae0a7989bacf776a2
ka02song <ka02song@gmail.com>
2023-11-30 14:16:25 +0000

    s3 python

--- rockets/hosting/views.py
+++ rockets/hosting/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from rocket_admin.models import *
 from django.utils import timezone
+from s3_functions import *
+
 
 
 # Create your views here.
