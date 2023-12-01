2138ecf9481fd122ca90a6d13bec7b4082214ffd
gyj1757 <gyj1757@gmail.com>
2023-12-01 07:55:45 +0000

    route53 레코드 생성 1

--- rockets/acm/acm_test.py
+++ rockets/acm/acm_test.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,redirect
+#from django.shortcuts import render,redirect
 import boto3
+import time
 #from rocket_admin.models import Serviceaws, Userinfo
 
 MAIN_DOMAIN = ".rockets-yj.com"
@@ -25,6 +26,62 @@ def request_certificate(service_name):
             # ACM이 DNS 확인을 위한 레코드를 생성할 것이므로,
             # 인증서를 사용하는 도메인의 DNS 레코드가 ACM에서 관리되어야 합니다.
 
-if __name__ == "__main__":
+    return certificate_arn
+
+def create_dns_record(service_name):
+    # ACM 인증서 정보 가져오기
+    acm_certificate_arn = request_certificate(service_name)
+    time.sleep(3)
+    acm_client = boto3.client('acm', region_name='us-east-1')  # 'your_region'을 사용하는 AWS 리전으로 변경
+    acm_certificate = acm_client.describe_certificate(CertificateArn=acm_certificate_arn)
+
+    # Route 53 클라이언트 생성
+    route53_client = boto3.client('route53')  # AWS 리전을 지정하지 않습니다.
+
+    # DNS 레코드 생성을 위한 정보 추출
+    domain_name = acm_certificate['Certificate']['DomainName']
+    validation_options = acm_certificate['Certificate']['DomainValidationOptions']
     
-    request_certificate("testservice")
+    for validation_option in validation_options:
+        record_name = validation_option['ResourceRecord']['Name']
+        record_value = validation_option['ResourceRecord']['Value']
+
+        # Route 53에 CNAME 레코드 생성
+        change_batch = {
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': record_name,
+                        'Type': 'CNAME',
+                        'TTL': 300,
+                        'ResourceRecords': [{'Value': record_value}]
+                    }
+                }
+            ]
+        }
+
+        # 'yj.com' 호스트 영역 ID로 변경
+        hosted_zone_id = "Z0958311YBP9BJL383S5"  
+        response = route53_client.change_resource_record_sets(
+            HostedZoneId=hosted_zone_id,
+            ChangeBatch=change_batch
+        )
+
+        # 생성된 CNAME 레코드 확인
+        change_info = response['ChangeInfo']
+        status = change_info['Status']
+
+        while status == 'PENDING':
+            time.sleep(10)
+            response = route53_client.get_change(Id=change_info['Id'])
+            change_info = response['ChangeInfo']
+            status = change_info['Status']
+
+        if status == 'INSYNC':
+            print(f"CNAME record created successfully for {record_name}")
+
+
+if __name__ == "__main__":
+    service_name = "acmtest1"
+    create_dns_record(service_name)
