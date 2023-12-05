d914ca5be6c61380ae90816cae5e581d48383761
gyj1757 <gyj1757@gmail.com>
2023-12-05 01:00:45 +0000

    helm 실행 후 폴더 삭제 추가

--- rockets/try/try.py
+++ rockets/try/try.py
@@ -10,7 +10,8 @@ def create_folder(folder_path):                                                 
     except FileExistsError:
         print(f"폴더 이미 존재: {folder_path}")
 
-def copy_folder(source_folder, destination_folder):                                    #templates 복사 
+
+def copy_folder(source_folder, destination_folder):                                    # templates 복사 
     try:
         shutil.copytree(source_folder, destination_folder)
         print(f"폴더 복사: {destination_folder}")
@@ -19,6 +20,20 @@ def copy_folder(source_folder, destination_folder):                             
     except FileExistsError:
         print(f"대상 폴더 이미 존재: {destination_folder}")
 
+def create_and_write_file(file_path, content):                                         # 파일 생성
+    with open(file_path, 'w') as file:
+        file.write(content)
+        print(f"파일 생성 및 문장 추가: {file_path}")
+
+
+def delete_folder(folder_path):                                                        # 서비스 생성 후 폴더 삭제
+    try:
+        shutil.rmtree(folder_path)
+        print(f"폴더 삭제 완료: {folder_path}")
+    except FileNotFoundError:
+        print(f"폴더를 찾을 수 없음: {folder_path}")
+    except Exception as e:
+        print(f"폴더 삭제 중 오류 발생: {e}")
 
 
 def values(service_name, port, docker_image):                                          # values.yaml 내용
@@ -26,22 +41,21 @@ def values(service_name, port, docker_image):                                   
     values = f"ingress:\n  name: \"{service_name}\"\n  namespace: \"{service_name}\"\n\nservice:\n  name: \"{service_name}\"     \n\napp:\n  name: \"{service_name}\"            \n\ncontainer:\n  name: \"{service_name}\"               \n  image: {docker_image}             \n  port: {port}                   \n\nnode:\n  name: \"{service_name}\"         \n  type: t3.medium        \n  dc: 2                  \n  size: 20               \n\nnamespace:\n  name: \"{service_name}\"  \n\ndeployment:\n  name: \"{service_name}\"\n  namespace: \"{service_name}\"\n  rep: 3"
     return values
 
-def chart(service_name, email):                                                         #Chart.yaml 내용
+
+def chart(service_name, email):                                                         # Chart.yaml 내용
 
     chart = f"apiVersion: v2\nname: {service_name}\ndescription: {service_name}\n\nkeywords:\n  - rocketdan\n  - rockets\n\nmaintainer:\n  - name: {service_name}\n    email: {email}\nengine: gotpl\n\ntype: application\nversion: 0.1.0"
     return chart
 
-def nodegroup(service_name):                                                            #nodegroup.yaml 내용
+
+def nodegroup(service_name):                                                            # nodegroup.yaml 내용
 
     ng = f"apiVersion: eksctl.io/v1alpha5\nkind: ClusterConfig\n\nmetadata:\n  name: rockets-eks\n  region: ap-northeast-2\n\nmanagedNodeGroups:\n  - name: {service_name}\n    labels:\n      nodegroup: {service_name}\n    instanceType: t3.medium\n    desiredCapacity: 1\n    volumesize: 20"
     return ng
 
-def create_and_write_file(file_path, content):
-    with open(file_path, 'w') as file:
-        file.write(content)
-        print(f"파일 생성 및 문장 추가: {file_path}")
 
-def create_eks_nodegroup(path, service_name):
+
+def create_eks_nodegroup(path, service_name):                                            # nodegroup 생성 명령어 실행
     command = f"eksctl create nodegroup --config-file=./{path}/nodegroup.yaml --include={service_name}"
     
     try:
@@ -49,6 +63,7 @@ def create_eks_nodegroup(path, service_name):
 
     except subprocess.CalledProcessError as e:
         print(f"Nodegroup 생성 명령어 실행 중 오류 발생: {e}")
+
 
 def create_service(service_name, image, port, email):
 
@@ -69,25 +84,29 @@ def create_service(service_name, image, port, email):
     create_and_write_file(chart_yaml, chart_content)
     create_and_write_file(nodegroup_yaml, nodegroup_content)
 
+
 def helm_delete(service_name):
     command = f"helm uninstall {service_name}"
     try:
         subprocess.run(command, shell=True, check=True)
+        helm_start(service_name)
 
     except subprocess.CalledProcessError as e:
         print(f"helm 삭제 중 오류 발생: {e}")
 
-def helm_start(service_name):
+
+def helm_start(service_name):                                          # helm repo가 잘 생성되면 폴더 삭제 
     command = f"helm install {service_name} {service_name}/"
     try:
         subprocess.run(command, shell=True, check=True)
+        delete_folder(service_name)
 
     except subprocess.CalledProcessError as e:
         print(f"helm 실행 중 오류 발생: {e}")
         helm_delete(service_name)    
 
 
-def get_load_balancer_dns(service_name):                    # 생성한 Load Balancer DNS 주소 가져오기 (load_balancer_dns로 저장)
+def get_load_balancer_dns(service_name):                               # 생성한 Load Balancer DNS 주소 가져오기 
     try:
         # AWS CLI를 사용하여 ELB 정보 조회
         command = f"aws elbv2 describe-load-balancers --names {service_name}"
@@ -96,7 +115,9 @@ def get_load_balancer_dns(service_name):                    # 생성한 Load Bal
         # JSON 형식으로 반환된 결과 파싱
         response_json = json.loads(result.stdout)
         load_balancer_dns = response_json['LoadBalancers'][0]['DNSName']
+        
         return load_balancer_dns
+    
     except subprocess.CalledProcessError as e:
         print(f"에러: {e}")
         return None
